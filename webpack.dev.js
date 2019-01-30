f235b7af787f7019aca7a43cc24e496882791050
dfoxpro <dfoxpro@gmail.com>
2019-01-30 15:47:12 -0500

    npm run dev now compile in www folder

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,11 @@
+const path = require('path');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 var zWCC = require('./webpack.common'); // zorroWebpackCommonConfigs
 
 zWCC.config.mode = 'development';
-zWCC.config.devServer = {
-	open: false
-};
+zWCC.config.output.path = path.resolve(__dirname, 'www');
+zWCC.config.module.rules[zWCC.index.htmlIndex].use[2].options.minimize = false;
 // zWCC.config.watch = true;
 // zWCC.config.plugins = [
 // 	new LiveReloadPlugin({})
